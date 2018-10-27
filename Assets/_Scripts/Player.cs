f8b2d76f175c31b52b2529b603d1b4fe354c6989
Ryan Kennedy <RIKennedy@me.com>
2018-10-27 00:18:48 -0700

    Added Unity Standard Assets, Added sphere and cube

---
+++ Assets/_Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update ()
+    {
+        print (CrossPlatformInputManager.GetAxis("Horizontal"));
+        print (CrossPlatformInputManager.GetAxis("Vertical"));
+    }
+}
